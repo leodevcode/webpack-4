1be44a367c6e3039120ecc87987519079c5fb3ff
Leonardo Gomez <contactoleodevcode@gmail.com>
2019-11-28 08:44:24 -0400

    Agregando configuraciones

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import '../assets/style/App.css'
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>¡Hola mundo!</h1>
+                <h2>desde React.js</h2>
+            </div>
+        );
+    };
+};
+
+export default App;
